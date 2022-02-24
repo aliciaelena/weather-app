cb7a3dcb2e5c5931e84266f229ea958cc0132985
aliciaelena <alicia.nagele@gmx.at>
2022-02-24 12:28:03 +0100

    Added bootstrap and Weather component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+      </div>
+    </div>
+  );
 }
 
 export default App;
