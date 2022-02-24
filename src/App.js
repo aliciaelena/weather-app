514cc59cf493d77738b7267c98a2bee0a3ed8052
aliciaelena <alicia.nagele@gmx.at>
2022-02-24 12:40:08 +0100

    Added basic HTML structure to Weather component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ function App() {
     <div className="App">
       <div className="container">
         <Weather />
+        <footer>
+          <a href="https://github.com/aliciaelena/weather-app" target="_blank">
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/alicia-nagele-789b5561?originalSubdomain=at"
+            target="_blank"
+          >
+            Alicia Nagele
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,28 @@ import React from "react";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <input type="search" placeholder="Enter city here..." />
+        <input type="submit" value="Search" />
+      </form>
       <h1>Vienna</h1>
+      <ul>
+        <li>Last updated:</li>
+        <li>Sunday, 19:01</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          6°C
+          <img src="http://openweathermap.org/img/wn/02n@2x.png" alt="cloudy" />
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Few clouds</li>
+            <li>Wind: 0 m/s</li>
+            <li>Humidity: 52%</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
